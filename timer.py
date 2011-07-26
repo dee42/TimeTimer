c9dd6da5b4a2a46186953d3be6af2bbff98c9f9c
David Moore <davidm@sjsoft.com>
2011-07-26 09:36:45 +0200

    Draw the clock face

--- timer.py
+++ timer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import sys, pygame
+import sys, pygame, numpy
 pygame.init()
 
-size = width, height = 320, 320
+width = height = 640
+size = width, height
 black = 0, 0, 0
 white = 255, 255, 255
 red = 255, 0, 0
@@ -14,6 +15,7 @@ screen = pygame.display.set_mode(size)
 # Create 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55 in black with background white
 font = pygame.font.Font(None, 20)
 numbers = [font.render(str(num), True, black, white) for num in range(0, 60, 5)]
+border_width=40
 
 while 1:
     for event in pygame.event.get():
@@ -22,8 +24,23 @@ while 1:
     # Draw the clock
     screen.fill(black)
     # White circle, filled
+    pygame.draw.ellipse(screen, white, pygame.Rect(0,0,width,height))
     # Black inner circle
+    pygame.draw.ellipse(screen,black,pygame.Rect(border_width,border_width,width-border_width*2,height-border_width*2), 3)
     # Mark 60 lines in black on the Circle, bigger ones on the 5 minute intervals
+    for tick in range(60):
+        big_tick = (tick % 5 == 0)
+        tick_x = numpy.sin(tick*numpy.pi/30)
+        tick_y = -numpy.cos(tick*numpy.pi/30)
+        if big_tick:
+            length = 20
+        else:
+            length = 10
+        start_x = tick_x*(width/2-border_width) + width/2
+        end_x = tick_x*(width/2-border_width-length) + width/2
+        start_y = tick_y*(height/2-border_width) + height/2
+        end_y = tick_y*(height/2-border_width-length) + height/2
+        pygame.draw.line(screen, black, (start_x, start_y), (end_x, end_y))
     # Place the text on the correct lines
     # Draw the arc in red on the clock
     pygame.display.flip()
